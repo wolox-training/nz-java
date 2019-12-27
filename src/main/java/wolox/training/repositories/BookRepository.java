ec3918cabee187b83a935fe8959a489097dc6537
nicozar95 <nzarewsky@gmail.com>
2019-12-27 12:48:10 -0300

    Update BookRepository

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,11 +1,13 @@
 package wolox.training.repositories;
 
+import java.util.Collection;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import wolox.training.models.Book;
 
-public interface BookRepository {
-  @Query("SELECT book from Book book WHERE book.author = :author")
+public interface BookRepository extends CrudRepository<Book, Long> {
+  @Query("SELECT book from Book book WHERE book.author = :author LIMIT 1")
   public Book findByAuthor(
       @Param("author") String author);
 }
