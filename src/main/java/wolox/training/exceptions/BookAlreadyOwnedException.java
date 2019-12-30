a051ee4912d0a425d313ea4da93573e23657f5f1
Nicolas Zarewsky <nzarewsky@gmail.com>
2019-12-30 15:30:34 -0300

    Add already own exception

---
+++ src/main/java/wolox/training/exceptions/BookAlreadyOwnedException.java
@@ -0,0 +1,14 @@
+package wolox.training.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import wolox.training.constants.ExceptionsConstants;
+
+@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = ExceptionsConstants.BOOK_ALREADY_OWN)
+
+public class BookAlreadyOwnedException extends Throwable {
+
+  public BookAlreadyOwnedException() {
+    super(ExceptionsConstants.BOOK_ALREADY_OWN, new Exception());
+  }
+}
