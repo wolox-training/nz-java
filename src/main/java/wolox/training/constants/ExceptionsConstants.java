9d39675e956239acdc305cc6c095ac721c9845f1
Nicolas Zarewsky <nzarewsky@gmail.com>
2019-12-30 12:49:38 -0300

    Global constants

---
+++ src/main/java/wolox/training/constants/ExceptionsConstants.java
@@ -0,0 +1,8 @@
+package wolox.training.constants;
+
+public class ExceptionsConstants {
+  private ExceptionsConstants() {}
+    public static final String BOOK_NOT_FOUND = "Book not found";
+    public static final String BOOK_ID_MISMATCH = "Book id mismatch";
+}
+
--- src/main/java/wolox/training/exceptions/BookIdMismatchException.java
+++ src/main/java/wolox/training/exceptions/BookIdMismatchException.java
@@ -2,10 +2,12 @@ package wolox.training.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import wolox.training.constants.ExceptionsConstants;
 
-@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = "Book and id mismatch")
+@ResponseStatus(code = HttpStatus.NOT_ACCEPTABLE, reason = ExceptionsConstants.BOOK_ID_MISMATCH)
 public class BookIdMismatchException extends RuntimeException {
+
     public BookIdMismatchException() {
-        super("Book and id mismatch", new Exception());
+        super(ExceptionsConstants.BOOK_ID_MISMATCH, new Exception());
     }
 }
