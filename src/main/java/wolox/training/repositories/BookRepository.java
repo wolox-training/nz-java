20fb197bb438565a2888385ac3c74701fea13fb7
Nicolas Zarewsky <nzarewsky@gmail.com>
2019-12-30 11:26:21 -0300

    Remove unnecesary manual query

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -7,7 +7,5 @@ import org.springframework.data.repository.query.Param;
 import wolox.training.models.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
-  @Query("SELECT book from Book book WHERE book.author = :author")
-  public Book findByAuthor(
-      @Param("author") String author);
+  public Book findByAuthor(String author);
 }
