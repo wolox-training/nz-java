7086b6c75532dee7039e69a524b53dd978b45292
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-02 15:07:54 -0300

    Add remove method

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -14,6 +14,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import wolox.training.exceptions.BookAlreadyOwnedException;
+import wolox.training.exceptions.BookNotFoundException;
 
 @Entity
 @NoArgsConstructor
@@ -45,6 +46,14 @@ public class User {
       throw new BookAlreadyOwnedException();
     } else {
       this.books.add(book);
+    }
+  }
+
+  public void removeBook(Book book) {
+    if(!this.books.contains(book)) {
+      throw new BookNotFoundException();
+    } else {
+      this.books.remove(book);
     }
   }
 
