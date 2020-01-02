ef252aab8d93d3aca322017abf3323b2361f9067
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-02 10:38:11 -0300

    Add create method in the UserController

--- src/main/java/wolox/training/controllers/UserController.java
+++ src/main/java/wolox/training/controllers/UserController.java
@@ -1,11 +1,16 @@
 package wolox.training.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import wolox.training.exceptions.UserNotFoundException;
+import wolox.training.models.Book;
 import wolox.training.models.User;
 import wolox.training.repositories.UserRepository;
 
@@ -20,5 +25,11 @@ public class UserController {
   public User findOne(@PathVariable Long id) {
     return userRepository.findById(id)
         .orElseThrow(UserNotFoundException::new);
+  }
+
+  @PostMapping
+  @ResponseStatus(HttpStatus.CREATED)
+  public User create(@RequestBody User user) {
+    return userRepository.save(user);
   }
 }
