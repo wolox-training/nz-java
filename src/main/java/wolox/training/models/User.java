5a3680546790e9fee30ef3ee65fdb4c789ab931a
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-02 11:38:09 -0300

    Change table name

--- src/main/java/wolox/training/models/User.java
+++ src/main/java/wolox/training/models/User.java
@@ -9,6 +9,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,7 @@ import lombok.Setter;
 import wolox.training.exceptions.BookAlreadyOwnedException;
 
 @Entity
+@Table(name = "users", schema = "public")
 @NoArgsConstructor
 public class User {
   @Id
