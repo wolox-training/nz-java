441e4e6359a8bde489e5f2f597a49c9f538bda51
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-02 11:38:09 -0300

    Change table name

--- src/main/java/wolox/training/models/Book.java
+++ src/main/java/wolox/training/models/Book.java
@@ -2,11 +2,13 @@ package wolox.training.models;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -42,7 +44,7 @@ public class Book {
   private String isbn;
 
   @ManyToOne
-  @JoinColumn(name="user_id")
+  @JoinColumn(name="users_id")
   @Getter @Setter
   private User user;
 
