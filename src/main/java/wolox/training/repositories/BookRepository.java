9a76a628e8a9d91af22b333da1e9b6585199f8ec
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-03 14:47:07 -0300

    List all books

--- src/main/java/wolox/training/repositories/BookRepository.java
+++ src/main/java/wolox/training/repositories/BookRepository.java
@@ -1,9 +1,11 @@
 package wolox.training.repositories;
 
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import wolox.training.models.Book;
 
 public interface BookRepository extends CrudRepository<Book, Long> {
   public Optional<Book> findByAuthor(String author);
+  public List<Book> findAll();
 }
