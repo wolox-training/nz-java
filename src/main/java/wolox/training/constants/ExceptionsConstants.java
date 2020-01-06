b13f3233c1911d5c8ea0793d91f44107c54bb764
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-06 11:25:48 -0300

    Add BookControllerConstants and remove unnecesary contructor in ExceptionConstants

--- src/main/java/wolox/training/constants/ExceptionsConstants.java
+++ src/main/java/wolox/training/constants/ExceptionsConstants.java
@@ -7,7 +7,5 @@ public class ExceptionsConstants {
   public static final String BOOK_ID_MISMATCH = "Book id mismatch";
   public static final String USER_NOT_FOUND = "User not found";
   public static final String USER_ID_MISMATCH = "User id mismatch";
-
-  private ExceptionsConstants() {}
 }
 
