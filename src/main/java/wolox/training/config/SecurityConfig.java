7e9b9b5fe114d5aab2635211c3cf8d4419d38c8f
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-09 16:27:33 -0300

    Change authentication

--- src/main/java/wolox/training/config/SecurityConfig.java
+++ src/main/java/wolox/training/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package wolox.training.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -31,8 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.authorizeRequests()
-        .anyRequest()
-        .authenticated()
+        .anyRequest().authenticated()
+          .antMatchers(HttpMethod.POST,"/api/users").permitAll()
+          .antMatchers(HttpMethod.POST, "api/books").permitAll()
         .and()
         .httpBasic()
         .and()
