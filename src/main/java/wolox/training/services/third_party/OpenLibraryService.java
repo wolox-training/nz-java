65a6403bab3292380290378113579aabf8d0000e
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-14 13:46:10 -0300

    This is not working men

--- src/main/java/wolox/training/services/third_party/OpenLibraryService.java
+++ src/main/java/wolox/training/services/third_party/OpenLibraryService.java
@@ -2,20 +2,27 @@ package wolox.training.services.third_party;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import javax.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import wolox.training.DTOs.BookDTO;
-import wolox.training.constants.services.OpenLibraryConstants;
 import wolox.training.exceptions.BookNotFoundException;
 
+@Resource
 public class OpenLibraryService {
+
+    @Value("${openlibrary.url}")
+    public String base_url;
 
     public BookDTO bookInfo(String isbn) throws JsonProcessingException, BookNotFoundException {
         RestTemplate restTemplate = new RestTemplate();
         String url = UriComponentsBuilder
-            .fromHttpUrl(OpenLibraryConstants.URL)
+            .fromHttpUrl(base_url)
             .queryParam("bibkeys", "ISBN:"+isbn)
             .queryParam("format","json")
             .queryParam("jscmd","data")
