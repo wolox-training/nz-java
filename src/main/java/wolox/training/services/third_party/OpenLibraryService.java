bbde0e86d5342f8c60dd4fb7aff48e66aeb68bb5
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-14 16:23:26 -0300

    Mock working

--- src/main/java/wolox/training/services/third_party/OpenLibraryService.java
+++ src/main/java/wolox/training/services/third_party/OpenLibraryService.java
@@ -2,16 +2,17 @@ package wolox.training.services.third_party;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import javax.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 import wolox.training.DTOs.BookDTO;
 import wolox.training.exceptions.BookNotFoundException;
 
+@Component
 public class OpenLibraryService {
 
     @Value("${openlibrary.url}")
