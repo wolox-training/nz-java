c6d682f93b98aa4c99b2380c09329d1b674e1aa7
nicozar95 <nzarewsky@gmail.com>
2020-01-17 16:57:54 -0300

    not very nice

--- src/test/java/wolox/training/repositories/BookRepositoryTest.java
+++ src/test/java/wolox/training/repositories/BookRepositoryTest.java
@@ -2,36 +2,24 @@ package wolox.training.repositories;
 
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace.NONE;
 
-import com.github.springtestdbunit.DbUnitTestExecutionListener;
-import com.github.springtestdbunit.TransactionDbUnitTestExecutionListener;
-import com.github.springtestdbunit.annotation.DatabaseSetup;
 import java.util.List;
+import javax.transaction.Transactional;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
-import wolox.training.controllers.UserController;
 import wolox.training.models.Book;
 
 @RunWith(SpringRunner.class)
-@ContextConfiguration
-@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class,
-    DirtiesContextTestExecutionListener.class,
-    TransactionalTestExecutionListener.class,
-    DbUnitTestExecutionListener.class })
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = NONE)
+@Transactional
 public class BookRepositoryTest {
 
     @Autowired
@@ -40,8 +28,12 @@ public class BookRepositoryTest {
     @Autowired
     private TestEntityManager entityManager;
 
+    @Before
+    public void setUp() {
+        entityManager.flush();
+    }
+
     @Test
-    @DatabaseSetup("BookRepositoryTest.xml")
     void whenInitializedByDbUnit_thenFindByPublisherAndGenreAndYear() {
         List<Book> books = bookRepository.findAll();
         //List<Book> books = bookRepository.findByPublisherAndGenreAndYear(
