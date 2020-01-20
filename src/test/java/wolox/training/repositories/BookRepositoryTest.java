2da23766b6fb6436e835c380cad440e1401d7765
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-20 16:53:17 -0300

    Fix initialize error

--- src/test/java/wolox/training/repositories/BookRepositoryTest.java
+++ src/test/java/wolox/training/repositories/BookRepositoryTest.java
@@ -6,18 +6,11 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
-import javax.annotation.Resource;
-import org.dbunit.database.DatabaseConfig;
-import org.junit.AfterClass;
 import org.junit.Before;
-import org.junit.BeforeClass;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
