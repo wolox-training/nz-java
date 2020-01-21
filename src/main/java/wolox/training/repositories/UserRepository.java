1a50cdf9a3871255bfcbb00488f7ac38b42c1d81
Nicolas Zarewsky <nzarewsky@gmail.com>
2020-01-21 12:12:34 -0300

    Add UserRepository Test

--- src/main/java/wolox/training/repositories/UserRepository.java
+++ src/main/java/wolox/training/repositories/UserRepository.java
@@ -1,5 +1,7 @@
 package wolox.training.repositories;
 
+import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +11,9 @@ import wolox.training.models.User;
 public interface UserRepository extends CrudRepository<User, Long> {
   public Optional<User> findByName(String name);
   public Optional<User> findByUsername(String username);
+
+  public List<User> findByBirthDateBetweenAndNameContainingIgnoreCase(
+      LocalDate firstDate,
+      LocalDate secondDate,
+      String name);
 }
--- src/test/java/wolox/training/repositories/UserRepositoryTest.java
+++ src/test/java/wolox/training/repositories/UserRepositoryTest.java
@@ -1,39 +1,39 @@
 package wolox.training.repositories;
 
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.github.database.rider.core.api.configuration.DBUnit;
 import com.github.database.rider.core.api.dataset.DataSet;
 import com.github.database.rider.spring.api.DBRider;
+import java.time.LocalDate;
 import java.util.List;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
-import wolox.training.models.Book;
+import wolox.training.models.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @DBRider
 @DBUnit(caseSensitiveTableNames = true, escapePattern = "\"?\"" )
 @ActiveProfiles("test")
-public class BookRepositoryTest {
+public class UserRepositoryTest {
 
-    @Autowired
-    private BookRepository bookRepository;
+  @Autowired
+  private UserRepository userRepository;
 
-    @Test
-    @DataSet("books.yml")
-    public void whenInitializedByDbUnit_thenFindByPublisherAndGenreAndYear() {
-        List<Book> bookList = bookRepository
-            .findByPublisherAndGenreAndYear("Penguin", "Fantasia","2000");
-        assertThat(bookList.size()).isEqualTo(2);
-        assertThat(bookList.get(0).getTitle()).isEqualTo("Harry Potter y el prisionero de Azkaban");
-        assertThat(bookList.get(1).getTitle()).isEqualTo("Misery");
-    }
+  @Test
+  @DataSet("users.yml")
+  public void whenInitializedByDbUnit_thenFindByBirthDateBetweenAndNameContainingIgnoreCase() {
+    List<User> userList = userRepository
+        .findByBirthDateBetweenAndNameContainingIgnoreCase(
+            LocalDate.of(1995,01,01),
+            LocalDate.of(1995,01,30),"epe");
+    assertThat(userList.size()).isEqualTo(1);
+    assertThat(userList.get(0).getName()).isEqualTo("Pepe");
+  }
 
 }
